b7ced4f4d4e1644f3bd5d181947ce71102aae7db
Antonio Vásquez Briones <niox33@gmail.com>
2019-02-19 01:02:12 -0300

    Code Uploaded

---
+++ different_zones_in_matrix.py
@@ -0,0 +1,60 @@
+'''
+For a given Matrix A, this code will
+compute how many separate areas are covered by the same number 
+
+--this was supposed to run inside a codility test, 
+so the same testcase has been included--
+
+Antonio Vasquez 20180219
+'''
+
+A = [[5, 4, 4],
+    [4, 3, 4],
+    [3, 2, 4],
+    [2, 2, 2],
+    [3, 3, 4],
+    [1, 4, 4],
+    [4, 1, 1]]
+
+def solution(A):
+        
+    # lets make a set with the color values and where their 
+    # points are located
+    color_set = {}
+    total_different_zones = 0
+    
+    for i in range(len(A)):
+        for j in range(len(A[i])):
+            
+            current_color = A[i][j]
+            
+            if current_color not in color_set:
+                color_set[A[i][j]] = []
+                
+            color_set[A[i][j]].append((i,j))
+
+    for color in color_set:
+        
+        # print('color: ' + str(color) + ' has ', end = ' ')
+
+        dot_list = color_set[color]
+        len_list = len(dot_list)
+
+        count_region = 1
+
+        for i in range(0,len_list-1):
+
+            same_row = dot_list[i][0] == dot_list[i+1][0]
+            same_col = dot_list[i][1] == dot_list[i+1][1]
+
+            if same_col or same_row:
+                count_region += 0
+            else:
+                count_region += 1    
+        
+        total_different_zones += count_region
+        # print(str(count_region) + ' zones')
+
+    return total_different_zones
+    
+print('the total different zones for the matrix A are: ' + str(solution(A)))
